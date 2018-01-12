e261bae10fc87f035bf66f1a540ef0a31de315d5
catherine wang <cawa@microsoft.com>
2018-01-12 09:39:54 -0800

    project source code

---
+++ aspnetcore1-web/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+
+namespace aspnetcore1_web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            HttpClient client = new HttpClient();
+            // client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = await client.GetAsync("your_web_service_URL");
+            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                throw new Exception("500 Internal Server Error occurred");
+            }
+
+            var responseBody = response.Content.ReadAsStringAsync().Result;
+
+            ViewBag.Number = responseBody;
+
+            return View();
+
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
